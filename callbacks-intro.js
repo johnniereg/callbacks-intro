78c7dec9ee56ee6f7f052bab2740d2cd60385dbe
Johnnie Regalado <johnniereg@gmail.com>
2017-11-02 18:26:54 +0000

    now using forEach and logging a string that gives answer

--- callbacks-intro.js
+++ callbacks-intro.js
@@ -1,14 +1,14 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(name, index, array) {
+    if (name === "Waldo") {
+      found(name, index, array);   // execute callback
     }
-  }
+  });
 }
 
-function actionWhenFound(i) {
-  console.log(i);
+function actionWhenFound(name, index, array) {
+  console.log(name, "was found at index", index, "in array:", array);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
